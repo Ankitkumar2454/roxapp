e775c067b09e56a0a9f841007c2ee1140c2ff43e
Ankitkumar2454 <ankitkr2454@gmail.com>
2025-10-16 16:40:14 +0530

    inital commit , will update UI in next commit

--- app/(personalChats)/ChatInPerson.tsx
+++ app/(personalChats)/ChatInPerson.tsx
@@ -9,7 +9,7 @@ import {
     Image,
     KeyboardAvoidingView,
     Platform,
-    SafeAreaView,
+
     ScrollView,
     StyleSheet,
     Text,
@@ -17,6 +17,7 @@ import {
     TouchableOpacity,
     View,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import io, { Socket } from 'socket.io-client';
 
 interface Message {
